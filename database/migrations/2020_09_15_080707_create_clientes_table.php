2827599d867c6cba9246572bb244bb2088e50978
Karlin Cruz <65685992+Karlinkd010@users.noreply.github.com>
2021-06-10 00:39:05 -0500

    Sistem
    Add

---
+++ database/migrations/2020_09_15_080707_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre');
+            $table->tinyInteger('Edad');
+            $table->bigInteger('Telefono');
+            $table->string('Direccion');
+            $table->string('Img');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
